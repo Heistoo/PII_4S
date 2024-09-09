8c91bb665942517ed8905be45bf66400d9136ee2
Lin <46348338+Heistoo@users.noreply.github.com>
2024-09-09 17:01:50 -0300

    change color of main screen

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
         useMaterial3: true,
       ),
       home: const AuthenticationScreen(),

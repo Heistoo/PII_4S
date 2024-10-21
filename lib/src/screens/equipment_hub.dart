2c9e5ffe7747a966bf6c13a0b447dbb546e98c0c
Lin <46348338+Heistoo@users.noreply.github.com>
2024-10-21 15:47:07 -0300

    adjust equipment hub columns

--- lib/src/screens/equipment_hub.dart
+++ lib/src/screens/equipment_hub.dart
@@ -25,65 +25,71 @@ class EquipamentosPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Image.asset(
-          'assets/imagens/Metro-SP_logo.png'
-        ),
+  return Scaffold(
+    appBar: AppBar(
+      title: Image.asset(
+        'assets/imagens/Metro-SP_logo.png',
+        height: 50,
       ),
-      body: ListView.builder(
-        itemCount: equipamentos.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Card(
-              child: Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Image.asset(
-                        'assets/extintor.png',
-                        height: 50,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'ID: ${equipamentos[index]['id']}',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          Text('Nome: ${equipamentos[index]['nome']}'),
-                          Text('Status: OK'),
-                          Text('Validade: 12/2025'),
-                          Text('Última Manut: 10/2023'),
-                        ],
-                      ),
-                    ],
-                  ),
-                  OverflowBar(
-                    alignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          // Ação ao clicar em "Info Detalhadas"
-                        },
-                        child: Text('Info Detalhadas'),
-                      ),
-                      ElevatedButton(
-                        onPressed: () {
-                          // Ação ao clicar em "Editar Equip"
-                        },
-                        child: Text('Editar Equip'),
-                      ),
-                    ],
-                  ),
-                ],
+    ),
+    body: LayoutBuilder(
+      builder: (context, constraints) {
+        // Define o número de colunas com base na largura da tela
+        int crossAxisCount = (constraints.maxWidth ~/ 200).toInt(); 
+        return GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: crossAxisCount, 
+            crossAxisSpacing: 10.0,
+            mainAxisSpacing: 10.0,
+            childAspectRatio: 1.0, 
+          ),
+          itemCount: equipamentos.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center, 
+                  children: [
+                    Image.asset(
+                      'assets/imagens/extintor.png',
+                      height: 50,
+                    ),
+                    SizedBox(height: 8),  // Espaçamento entre imagem e informações
+                    Text(
+                      'ID: ${equipamentos[index]['id']}',
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    Text('Nome: ${equipamentos[index]['nome']}'),
+                    Text('Status: OK'),
+                    Text('Validade: 12/2025'),
+                    Text('Última Manut: 10/2023'),
+                    Spacer(), 
+                    OverflowBar(
+                      alignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {
+                            // Ação ao clicar em "Info Detalhadas"
+                          },
+                          child: Text('Info Detalhadas'),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {
+                            // Ação ao clicar em "Editar Equip"
+                          },
+                          child: Text('Editar Equip'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          );
-        },
-      ),
-    );
-  }
+            );
+          },
+        );
+      },
+    ),
+  );
+}
 }

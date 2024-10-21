d150838c460026fa559797eebcf8492ee925f109
Lin <46348338+Heistoo@users.noreply.github.com>
2024-10-21 16:26:06 -0300

    add commentary regarding riverpod benefits

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'src/screens/login_screen.dart';
 import 'src/screens/main_hub.dart';
 import 'src/screens/equipment_hub.dart';
 
+//Para o projeto, iremos utilizar da framework Riverpod para poder gerenciar os estados de autenticações de forma mais fácil e organizada.
+//Como o projeto é grande e demasidamente complexo, precisamos gerenciar os componentes de forma eficiente trazendo eles globalmente entre as telas, evitando também a repetição de código e trazendo o acesso a estados através de um arquivo único.
+//O riverpod separa a parte lógica do código da parte gráfica, fazendo com que os widgets só mostrem o que será exibido na tela, enquanto a parte lógica fica em outro arquivo, facilitando a manutenção do código.
+
 void main() {
   runApp(
     //function from riverpod framework para gerenciamento de estados globais
